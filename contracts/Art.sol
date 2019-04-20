c7969a75a89de3508b48878a9467613a23f79c73
Evis Cheng <e920528@gmail.com>
2019-04-20 23:14:16 +0800

    [New Feature] First version

---
+++ contracts/Art.sol
@@ -0,0 +1,57 @@
+pragma solidity ^0.5.0;
+
+import "./Transaction.sol";
+
+contract Arts {
+
+    struct Art {
+      string artName;
+      uint artId;
+      address owner;
+      Transaction[] history;
+    }
+    Art[] arts;
+
+    mapping(uint => Art) artInfo;
+    mapping(uint => Transaction) transactionInfo;
+
+    mapping(uint => address payable) artOwner;
+
+    mapping(address => People) peopleInfo;
+
+    struct People {
+      uint id;
+      string name;
+      Art[] ownedArts;
+    }
+    
+    
+    constructor(Art[] memory _array) public {
+		  arts = _array;
+    }
+    
+    function getArtInfo(uint _artId) public view returns(uint, string memory, string memory, Transaction[] memory) {
+        Art memory art = artInfo[_artId];
+        return (art.artId, art.artName, peopleInfo[art.owner].name, art.history);
+    }
+
+    function Time_call() public view returns (uint256){
+        return now;
+    }
+
+    function applyTransaction(uint _transactionId, address payable oldOwner, address payable newOwner, uint art, uint _price) public {
+        require(oldOwner != newOwner);
+        require(_price > 0);
+        require(artInfo[art].owner == oldOwner);
+        
+        for(uint i = 0; i < peopleInfo[oldOwner].ownedArts.length; i++){
+          if(peopleInfo[oldOwner].ownedArts[i].artId == artInfo[art].artId) {
+            delete peopleInfo[oldOwner].ownedArts[i];
+          }
+        }
+        peopleInfo[newOwner].ownedArts.push(artInfo[art]);
+        Transaction t = (new Transaction)(_transactionId, artInfo[art].artId, artInfo[art].artName, _price, Time_call(), oldOwner, newOwner);
+        artInfo[art].history.push(t);
+    }
+    
+}
---
+++ contracts/Transaction.sol
@@ -0,0 +1,23 @@
+contract Transaction {
+  uint transactionId;
+  uint artId;
+  string artName;
+  uint price;
+  uint256 date;
+  address payable old_owner;
+  address payable new_owner;
+  constructor(uint _transactionId, uint _artId, string memory _artName, uint _price, uint256 _date, address payable _old_owner, address payable _new_owner) public {
+    transactionId = _transactionId;
+    artId = _artId;
+    artName = _artName;
+    price = _price;
+    date = _date;
+    old_owner = _old_owner;
+    new_owner = _new_owner;
+  }
+  
+
+  function getTransactionInfo() public view returns(uint, uint, string memory, uint, uint256, address, address) {
+    return (transactionId, artId, artName, price, date, old_owner, new_owner);
+  }
+}
